b83485d68cccf96d29d3b82d209db5a1c71e91a7
cxhy <979009228@qq.com>
2018-10-17 21:38:06 +0800

    daily

--- Animal.pm
+++ Animal.pm
@@ -1,11 +1,20 @@
-
 package Animal;
+#use parent qw(LivingCreature);
+sub name {
+    my $self = shift;
+    $$self;
+}
 
-use parent qw(LivingCreature);
-# sub speak{
-#     my $class = shift;
-#     print "a $class goes ",$class->sound,"!\n";
-# }
+sub named{
+    my $class = shift;
+    my $name = shift;
+    bless \$name, $class;
+}
+
+ sub speak{
+     my $class = shift;
+     print "a $class goes ",$class->sound,"!\n";
+ }
 
 sub sound{
     die "you have not define the sound() in sub class\n";
